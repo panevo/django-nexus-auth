0d3d3f6388d1dc7235d01d5e63336ba982431da7
gabriel-tan <gabriel.tan@panevo.com>
2025-03-28 11:45:39 -0700

    docs: Add function docs for factory classes

--- nexus_auth/providers/factory.py
+++ nexus_auth/providers/factory.py
@@ -6,13 +6,31 @@ from django.utils.module_loading import import_string
 
 
 class ObjectFactory:
+    """Factory for creating objects."""
+
     def __init__(self):
+        """Initialize the factory with no builders."""
         self._builders = {}
 
     def register_builder(self, key, builder):
+        """Register a builder for a specific key.
+
+        Args:
+            key: The key for the builder.
+            builder: The builder to register.
+        """
         self._builders[key] = builder
 
     def create(self, key, **kwargs):
+        """Create an object using the registered builder.
+
+        Args:
+            key: The key for the builder.
+            **kwargs: Keyword arguments to pass to the builder.
+
+        Returns:
+            The created object.
+        """
         builder = self._builders.get(key)
         if not builder:
             raise ValueError(key)
@@ -23,6 +41,15 @@ class IdentityProviderFactory(ObjectFactory):
     """Factory for identity providers."""
 
     def get(self, provider_type: str, **kwargs) -> Optional[OAuth2IdentityProvider]:
+        """Get an identity provider instance.
+
+        Args:
+            provider_type: The type of provider to get.
+            **kwargs: Keyword arguments to pass to the builder.
+
+        Returns:
+            Optional[OAuth2IdentityProvider]: The identity provider instance.
+        """
         return self.create(provider_type, **kwargs)
 
 
