b57e6551c7dc77991314666240586ae4f96ebc3a
gabriel-tan <gabriel.tan@panevo.com>
2025-07-28 13:31:21 -0700

    chore: Update version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-nexus-auth",
-    version="0.1.4",
+    version="0.1.6",
     packages=find_packages(),
     install_requires=[
         "Django>=4.2.19",
